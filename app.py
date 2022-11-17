650ae9f80673e742c625a63992e0a72ac6e27bb1
Svetlana <36902132+svkote@users.noreply.github.com>
2022-11-17 13:48:12 +0300

    add base template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory
+
 # from functions import ...
 
 POST_PATH = "posts.json"
@@ -6,24 +7,34 @@ UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
 
+menu = [
+    {'url': '/', 'title': 'Главная'},
+    {'url': '/post', 'title': 'Добавить пост'},
+]
+
 
 @app.route("/")
 def page_index():
-    pass
+    """ главная страница (поиск постов)"""
+    # s = request.args['s']
+    return render_template('index.html', menu=menu)
 
 
 @app.route("/list")
 def page_tag():
+    """страничка ленты по тегу"""
     pass
 
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_form():
+    """страничка добавления поста"""
     pass
 
 
 @app.route("/post", methods=["POST"])
 def page_post_upload():
+    """страничка после добавления поста"""
     pass
 
 
@@ -33,4 +44,3 @@ def static_dir(path):
 
 
 app.run()
-
